7b97d622237eb74829884148b240d3754fb95cac
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-04-21 21:27:21 +0530

    Update the chart to show actual data.

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import  ReactDOM  from "react-dom/client";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -12,6 +13,8 @@ import {
 import { Line } from "react-chartjs-2";
 import {useFaker} from 'react-fakers';
 import faker from 'faker'
+import "../styles/Chart.css"
+import CustomTooltip from "./CustomTooltip";
 
 ChartJS.register(
   CategoryScale,
@@ -23,13 +26,37 @@ ChartJS.register(
   Legend
 );
 
-const Chart = (START_DATE, END_DATE) => {
+
+const Chart = ({START_DATE, END_DATE, allData}) => {
+  const allAsteroidData = allData.near_earth_objects;
+  const dates = Object.keys(allAsteroidData);
+  dates.sort();
+  const arrayLengths = dates.map(date => allAsteroidData[date].length);
   
   const options = {
     responsive: true,
     plugins: {
       legend: {
         position: "top",
+      },
+      tooltip: {
+        enabled: false,
+        position: 'nearest',
+        external: (tooltipModel) => {
+          const {chart, tooltip} = tooltipModel;
+          const tooltipEl = document.getElementById('chartjs-tooltip');
+          const root = ReactDOM.createRoot(tooltipEl);
+          root.render(<CustomTooltip 
+            chart = {chart}
+            tooltip = {tooltip}
+            allAsteroidData = {allAsteroidData}
+            dates = {dates}
+          />);
+
+          const tooltipComponent = <CustomTooltip chart = {chart} tooltip = {tooltip} />;
+
+          return '';
+        }
       },
       title: {
         display: true,
@@ -40,16 +67,16 @@ const Chart = (START_DATE, END_DATE) => {
 
   const labels = [];
   
-  for (let i = START_DATE; i < END_DATE; i++) {
+  for (let i = (new Date(START_DATE)).getDate(); i <= (new Date(END_DATE)).getDate(); i++) {
     labels.push(i);
   }
-  
+
   const data = {
     labels,
     datasets: [
       {
         label: "Asteroid chart",
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 3 })),
+        data: arrayLengths, 
         borderColor: "rgb(53, 162, 235)",
         backgroundColor: "rgba(53, 162, 235, 0.5)",
       },
@@ -57,7 +84,10 @@ const Chart = (START_DATE, END_DATE) => {
   };
 
   return (
-    <Line options={options} data={data} />
+    <div id="chart">
+      <Line options={options} data={data} />
+      <div id="chartjs-tooltip"></div>
+    </div>
   )
 }
 
